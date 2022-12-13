32a18872f33a6b2c884b8c27749ca9b77eb76371
Christopher <christopherocity@gmail.com>
2022-12-13 15:17:51 -0500

    refactor

--- test.js
+++ test.js
@@ -4,16 +4,15 @@ function sudoku(puzzle) {
             for (let j = 0; j < 9; j++) {
                 if (puzzle[i][j] === 0) {
                     let candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-                    for (let z = 0; z < 9; z++) {
-                        candidates = candidates.filter(num => ![puzzle[i][z], puzzle[z][j]].includes(num));
-                    }
-
                     const iMax = Math.ceil((i + 1) / 3) * 3;
                     const jMax = Math.ceil((j + 1) / 3) * 3;
                     for (let x = iMax - 3; x < iMax; x++) {
                         for (let y = jMax - 3; y < jMax; y++) {
                             candidates = candidates.filter(num => num !== puzzle[x][y]);
                         }
+                    }
+                    for (let z = 0; z < 9; z++) {
+                        candidates = candidates.filter(num => ![puzzle[i][z], puzzle[z][j]].includes(num));
                     }
 
                     puzzle[i][j] = candidates.length === 1 ? candidates[0] : 0;
