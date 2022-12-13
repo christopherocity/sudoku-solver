bdca2401bc3b8b2be0f529680b237e9845625f91
Christopher <christopherocity@gmail.com>
2022-12-12 22:16:53 -0500

    javascript-update

--- test.js
+++ test.js
@@ -8,21 +8,19 @@ function sudoku(puzzle) {
             }
         }
     }
-
     return puzzle;
-
 }
 
 function solveSpace(puzzle, i, j) {
-    let candidates = [1,2,3,4,5,6,7,8,9];
+    let candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     for (let z = 0; z < 9; z++) {
         candidates = candidates.filter(num => num !== puzzle[z][j]);
         candidates = candidates.filter(num => num !== puzzle[i][z]);
     }
 
-    const iMax = Math.ceil((i+1) / 3) * 3;
-    const jMax = Math.ceil((j+1) / 3) * 3;
+    const iMax = Math.ceil((i + 1) / 3) * 3;
+    const jMax = Math.ceil((j + 1) / 3) * 3;
     for (let x = iMax - 3; x < iMax; x++) {
         for (let y = jMax - 3; y < jMax; y++) {
             candidates = candidates.filter(num => num !== puzzle[x][y]);
@@ -34,19 +32,18 @@ function solveSpace(puzzle, i, j) {
 }
 
 function printPuzzle(puzzle) {
-    console.log("\n\n");
     for (let i = 0; i < puzzle.length; i++) {
         console.log(puzzle[i].toString())
     }
 }
 
 printPuzzle(sudoku([
-    [5,3,0,0,7,0,0,0,0],
-    [6,0,0,1,9,5,0,0,0],
-    [0,9,8,0,0,0,0,6,0],
-    [8,0,0,0,6,0,0,0,3],
-    [4,0,0,8,0,3,0,0,1],
-    [7,0,0,0,2,0,0,0,6],
-    [0,6,0,0,0,0,2,8,0],
-    [0,0,0,4,1,9,0,0,5],
-    [0,0,0,0,8,0,0,7,9]]));
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]]));
